ea680e37fe537fa4a092662df590ed561b5af5f4
William <william.dower@griffithuni.edu.au>
2020-04-17 13:38:08 +1000

    Added priority queue template

--- main.cpp
+++ main.cpp
@@ -52,6 +52,78 @@ public:
     }
 };
 
+template <class T>
+class newPriorityQueue {
+private:
+    T *arr;
+    bool isScott;
+    unsigned long maxSize;
+    unsigned long currentSize;
+
+     T getPriority(T input) {
+        if (isScott) {
+            return input;
+        }
+        else {
+            T sum = 0;
+            while (input != 0) {
+                sum += input % 10;
+                input /= 10;
+            }
+            return sum;
+        }
+    }
+
+public:
+    //Children of a[n] at a[(2*n)+1] and a[(2*n)+2]
+    //Parent of a[n] at a[n/2]
+    void push(T input) {
+        unsigned long location = currentSize;
+        arr[currentSize] = input;
+        currentSize++;
+        while (getPriority(arr[location/2]) > this->getPriority(arr[location])) {
+            T temp = arr[location];
+            arr[location] = arr[location/2];
+            arr[location/2] = temp;
+            location /= 2;
+        }
+    }
+
+    T pop() {
+         T returnValue = arr[0];
+         arr[0] = 0;
+         unsigned long location = 0;
+         while (arr[(2*location)+1] != 0 && arr[(2*location)+2] != 0) {
+             T leftChildPriority = this->getPriority(arr[(2*location)+1]);
+             T rightChildPriority = this->getPriority(arr[2*location]+2);
+
+             if (leftChildPriority < rightChildPriority) {
+                 arr[location] = arr[(2*location)+2];
+                 arr[(2*location)+2] = 0;
+                 location = (2*location)+2;
+             }
+             else {
+                 arr[location] = arr[(2*location)+1];
+                 arr[(2*location)+1] = 0;
+                 location = (2*location)+1;
+             }
+         }
+         return returnValue;
+    }
+
+    newPriorityQueue() = delete;
+
+    newPriorityQueue(const bool isScottsQueue, const long n) {
+        maxSize = n;
+        currentSize = 0;
+        isScott = isScottsQueue;
+        arr = new T [n]{0};
+    }
+    ~newPriorityQueue() {
+        delete [] arr;
+    }
+};
+
 std::string cleanGetline(std::ifstream & input) {
     std::string returnString;
     getline(input, returnString);
