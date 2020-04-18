97cb3e5bc1ed81a53b629682f79b388e759db180
William <william.dower@griffithuni.edu.au>
2020-04-19 01:57:34 +1000

    IT WORKS!!!!!!!!!!!!!!!!!

--- main.cpp
+++ main.cpp
@@ -1,71 +1,36 @@
 //
-// Created by William on 15/04/2020.
+// Created by William on 18/04/2020.
 //
 
 #include <iostream>
 #include <fstream>
 #include <sstream>
 
-template <class T>
-class sharedObject {
-private:
-    T **storedObject;
-public:
-    bool checkObjectExists() {
-        return (*storedObject != nullptr);
-    }
-
-    T getObject() {
-        if (this->checkObjectExists()) {
-            return **storedObject;
-        }
-        else {
-            return -1;
-        }
-    }
-
-    T operator == (const T & otherValue) {
-        if (**storedObject == otherValue) {
-            return true;
-        }
-    }
-
-    void operator = (const T toSet) {
-        **storedObject = toSet;
-    }
-
-    void destroy() {
-        delete *storedObject;
-        *storedObject = nullptr;
-    }
-
-    sharedObject() {
-        storedObject = new T*;
-        *storedObject = new T;
-    }
-    sharedObject(sharedObject<T> & existingObject) {
-        storedObject = existingObject.storedObject;
-    }
-
-    ~sharedObject() {
-        delete storedObject;
-    }
+struct node {
+    unsigned long ballValue;
+    unsigned long scottQueuePos;
+    unsigned long rustyQueuePos;
 };
 
-template <class T>
 class newPriorityQueue {
 private:
-    T *arr;
+    node **arr;
     bool isScott;
     unsigned long maxSize;
-    unsigned long currentSize;
+    long long currentSize; //Must be signed
 
-     T getPriority(T input) {
+    unsigned long getPriority(const unsigned long location) {
+        // Check if outside array or nothing is there
+        // Will not segfault due to optimised if checking on OR statement
+        if (location >= maxSize || arr[location] == nullptr) {
+            return 0;
+        }
+        unsigned long input = arr[location]->ballValue;
         if (isScott) {
             return input;
         }
         else {
-            T sum = 0;
+            unsigned long sum = 0;
             while (input != 0) {
                 sum += input % 10;
                 input /= 10;
@@ -74,50 +39,321 @@ private:
         }
     }
 
-public:
-    //Children of a[n] at a[(2*n)+1] and a[(2*n)+2]
-    //Parent of a[n] at a[n/2]
-    void push(T input) {
-        unsigned long location = currentSize;
-        arr[currentSize] = input;
-        currentSize++;
-        while (getPriority(arr[location/2]) > this->getPriority(arr[location])) {
-            T temp = arr[location];
-            arr[location] = arr[location/2];
-            arr[location/2] = temp;
-            location /= 2;
+    unsigned long getBallValue(const unsigned long location) {
+        if (location >= maxSize || arr[location] == nullptr) {
+            return 0;
+        }
+        else {
+            return arr[location]->ballValue;
         }
     }
 
-    T pop() {
-         T returnValue = arr[0];
-         arr[0] = 0;
-         unsigned long location = 0;
-         while (arr[(2*location)+1] != 0 && arr[(2*location)+2] != 0) {
-             T leftChildPriority = this->getPriority(arr[(2*location)+1]);
-             T rightChildPriority = this->getPriority(arr[2*location]+2);
-
-             if (leftChildPriority < rightChildPriority) {
-                 arr[location] = arr[(2*location)+2];
-                 arr[(2*location)+2] = 0;
-                 location = (2*location)+2;
-             }
-             else {
-                 arr[location] = arr[(2*location)+1];
-                 arr[(2*location)+1] = 0;
-                 location = (2*location)+1;
-             }
-         }
-         return returnValue;
+    void swap(const unsigned long locationOne, const unsigned long locationTwo) {
+        node *temp = arr[locationOne];
+        arr[locationOne] = arr[locationTwo];
+        arr[locationTwo] = temp;
+        setCurrentPos(locationOne);
+        setCurrentPos(locationTwo);
     }
+
+    //Children of a[n] at a[(2*n)+1] and a[(2*n)+2]
+    //Parent of a[n] at a[n/2]
+    static unsigned long parent(unsigned long input) {
+        if (input == 1) {
+            return 1;
+        }
+        else {
+            return input / 2;
+        }
+    }
+
+    static unsigned long leftChild(unsigned long input) {
+        return (2*input);
+    }
+
+    static unsigned long rightChild(unsigned long input) {
+        return (2*input)+1;
+    }
+
+    bool isLeaf(const unsigned long location) {
+        return (arr[leftChild(location)] == nullptr && arr[rightChild(location)]);
+    }
+
+    void setCurrentPos(const unsigned long location) {
+        if (arr[location] == nullptr) {
+            return;
+        }
+        if (isScott) {
+            arr[location]->scottQueuePos = location;
+        }
+        else {
+            arr[location]->rustyQueuePos = location;
+        }
+    }
+
+    unsigned long findNextSwap(unsigned long location) {
+        auto parentPriority = getPriority(parent(location));
+        auto currentPriority = getPriority(location);
+        auto leftChildPriority = getPriority(leftChild(location));
+        auto rightChildPriority = getPriority(rightChild(location));
+
+        if (parentPriority < currentPriority) {
+            return parent(location);
+        }
+
+        if (parentPriority == currentPriority && currentPriority != 0 && getBallValue(location) != getBallValue(parent(location))) {
+            if (getBallValue(parent(location)) < getBallValue(location)) {
+                return parent(location);
+            }
+            else {
+                return location;
+            }
+        }
+
+        if (leftChildPriority > currentPriority || rightChildPriority > currentPriority) {
+            if (leftChildPriority > rightChildPriority) {
+                return leftChild(location);
+            }
+            else if (rightChildPriority > leftChildPriority) {
+                return rightChild(location);
+            }
+            else if (getBallValue(leftChild(location)) > getBallValue(rightChild(location))) {
+                return leftChild(location);
+            }
+            else {
+                return rightChild(location);
+            }
+        }
+
+        if ((leftChildPriority == currentPriority || rightChildPriority == currentPriority) && currentPriority != 0) {
+            if (leftChildPriority > rightChildPriority) {
+                if (getBallValue(leftChild(location)) > getBallValue(location)) {
+                    return leftChild(location);
+                }
+                else {
+                    return location;
+                }
+            }
+            else if (rightChildPriority > leftChildPriority) {
+                if (getBallValue(rightChild(location)) > getBallValue(location)) {
+                    return rightChild(location);
+                }
+                else {
+                    return location;
+                }
+            }
+            else {
+                if (getBallValue(location) > getBallValue(leftChild(location)) && getBallValue(location) > getBallValue(rightChild(location))) {
+                    return location;
+                }
+                else if (getBallValue(leftChild(location)) > getBallValue(rightChild(location))) {
+                    return leftChild(location);
+                }
+                else {
+                    return rightChild(location);
+                }
+            }
+        }
+
+        return location;
+    }
+
+    void maxHeapify(unsigned long location) {
+        auto nextLocation = findNextSwap(location);
+        while (location != nextLocation) {
+            swap(location, nextLocation);
+            location = nextLocation;
+            nextLocation = findNextSwap(location);
+        }
+
+        /*while (true) {
+            auto parentPriority = getPriority(parent(location));
+            auto currentPriority = getPriority(location);
+            auto leftChildPriority = getPriority(leftChild(location));
+            auto rightChildPriority = getPriority(rightChild(location));
+
+            if (parentPriority < currentPriority) {
+                swap(location, parent(location));
+                location = parent(location);
+                continue;
+            }
+            else if (currentPriority == parentPriority && currentPriority != 0 && getBallValue(location) != getBallValue(parent(location))) {
+                if (getBallValue(parent(location)) < getBallValue(location)) {
+                    swap(location, parent(location));
+                    location = parent(location);
+                }
+                else {
+                    break;
+                }
+            }
+            else if (leftChildPriority > currentPriority || rightChildPriority > currentPriority) {
+                if (leftChildPriority > rightChildPriority) {
+                    swap(location, leftChild(location));
+                    location = leftChild(location);
+                    continue;
+                }
+                else {
+                    swap(location, rightChild(location));
+                    location = rightChild(location);
+                    continue;
+                }
+            }
+            else {
+                break;
+            }
+        }*/
+    }
+
+public:
+
+    void push(node *newNode) {
+        unsigned long location = currentSize+1;
+        arr[location] = newNode;
+        currentSize++;
+        while (getPriority(parent(location)) < getPriority(location)) {
+            swap(location, parent(location));
+            location = parent(location);
+        }
+        if (getPriority(parent(location)) == getPriority(location)) {
+            if (getBallValue(parent(location)) < getBallValue(location)) {
+                swap(location, parent(location));
+                location = parent(location);
+            }
+        }
+        if (isScott) {
+            arr[location]->scottQueuePos = location;
+        }
+        else {
+            arr[location]->rustyQueuePos = location;
+        }
+    }
+
+    unsigned long pop(unsigned long & otherPos) {
+
+        unsigned long returnValue = arr[1]->ballValue;
+        if (isScott) {
+            otherPos = arr[1]->rustyQueuePos;
+        }
+        else {
+            otherPos = arr[1]->scottQueuePos;
+        }
+        arr[1] = nullptr;
+        swap(1, currentSize);
+        currentSize--;
+        unsigned long location = 1;
+
+        // Finish heapifying when:
+        // a) Location has higher priority than children
+        // b) It is a leaf node
+
+        maxHeapify(location);
+
+        /*while (true) {
+            if (isLeaf(location)) {
+                break;
+            }
+
+            unsigned long currentPriority = getPriority(location);
+            unsigned long leftChildPriority = getPriority(leftChild(location));
+            unsigned long rightChildPriority = getPriority(rightChild(location));
+
+            if (currentPriority >= leftChildPriority && currentPriority >= rightChildPriority) {
+                break;
+            }
+            else if (leftChildPriority > rightChildPriority) {
+                swap(location, leftChild(location));
+                location = leftChild(location);
+            }
+            else {
+                swap(location, rightChild(location));
+                location = rightChild(location);
+            }
+        }*/
+        return returnValue;
+    }
+
+    void repair(unsigned long location) {
+        swap(location, currentSize);
+        delete arr[currentSize];
+        arr[currentSize] = nullptr;
+        currentSize--;
+
+        // Finish repairing when:
+        // a) It's priority is greater than its children AND its priority is less than its parent
+        // b) It's priority is greater than its children AND it is root
+        // c) It's priority is less than its parent AND it is leaf
+
+        maxHeapify(location);
+    }
+
+        /*while (true) {
+
+            auto parentPriority = getPriority(parent(location));
+            auto currentPriority = getPriority(location);
+            auto leftChildPriority = getPriority(leftChild(location));
+            auto rightChildPriority = getPriority(rightChild(location));
+
+            // If current location is the root node
+            if (location == 0 && currentPriority >= leftChildPriority && currentPriority >= rightChildPriority) {
+                break;
+            }
+            else if (location == 0 && leftChildPriority > rightChildPriority) {
+                swap(location, leftChild(location));
+                location = leftChild(location);
+                continue;
+            }
+            else if (location == 0 && rightChildPriority > leftChildPriority) {
+                swap(location, rightChild(location));
+                location = rightChild(location);
+                continue;
+            }
+
+            // If current location is a leaf node
+            if (isLeaf(location) && currentPriority <= parentPriority) {
+                break;
+            }
+            else if (isLeaf(location) && currentPriority > parentPriority) {
+                swap(location, parent(location));
+                location = parent(location);
+                continue;
+            }
+
+            // If it is NOT root OR leaf
+            if (parentPriority < currentPriority) {
+                swap(location, parent(location));
+                location = parent(location);
+                continue;
+            }
+            else if (leftChildPriority > currentPriority || rightChildPriority > currentPriority) {
+                if (leftChildPriority > rightChildPriority) {
+                    swap(location, leftChild(location));
+                    location = leftChild(location);
+                    continue;
+                }
+                else {
+                    swap(location, rightChild(location));
+                    location = rightChild(location);
+                    continue;
+                }
+            }
+            else {
+                break;
+            }
+        }
+
+    }*/
 
     newPriorityQueue() = delete;
 
     newPriorityQueue(const bool isScottsQueue, const long n) {
-        maxSize = n;
+        maxSize = n+1;
         currentSize = 0;
         isScott = isScottsQueue;
-        arr = new T [n]{0};
+        arr = new node* [n+1];
+        for (int i=0;i<n;i++) {
+            arr[i] = nullptr;
+        }
+
     }
     ~newPriorityQueue() {
         delete [] arr;
@@ -133,25 +369,70 @@ std::string cleanGetline(std::ifstream & input) {
     return returnString;
 }
 
-int main() {
+int main(int argc, char **argv) {
 
-    sharedObject<int> newObj;
+    if (argc != 2) {
+        std::cout << "Incorrect number of arguments" << std::endl;
+        exit(1);
+    }
 
-    newObj = 4;
+    std::ifstream input(argv[1]);
+    std::ofstream output("output.txt");
 
-    std::cout << newObj.getObject() << std::endl;
+    std::stringstream testCasesLine(cleanGetline(input));
+    int testCases;
+    testCasesLine >> testCases;
 
-    sharedObject<int> secondObj(newObj);
+    for (int testCase=1;testCase<=testCases;testCase++) {
+        std::stringstream getDetailsLine(cleanGetline(input));
+        std::stringstream getBallsLine(cleanGetline(input));
+        std::string flipResult = cleanGetline(input);
 
-    std::cout << secondObj.getObject() << std::endl;
+        int n, k;
+        getDetailsLine >> n >> k;
 
-    secondObj = 3;
+        newPriorityQueue scottQueue(true, n);
+        newPriorityQueue rustyQueue(false, n);
 
-    std::cout << newObj.getObject() << std::endl;
+        for (int i=0;i<n;i++) {
+            auto *nextNode = new node;
+            getBallsLine >> nextNode->ballValue;
 
-    newObj.destroy();
+            scottQueue.push(nextNode);
+            rustyQueue.push(nextNode);
+        }
 
-    std::cout << secondObj.getObject() << std::endl;
+        unsigned long ballsLeft = n;
+        bool scottsTurn = (flipResult == "HEADS");
+        unsigned long long scottScore = 0, rustyScore = 0;
+        unsigned long otherPos;
+        while (ballsLeft > 0) {
+            if (scottsTurn) {
+                unsigned long leftThisTurn = k;
+                while (leftThisTurn > 0 && ballsLeft > 0) {
+                    scottScore += scottQueue.pop(otherPos);
+                    rustyQueue.repair(otherPos);
+                    ballsLeft--;
+                    leftThisTurn--;
+                }
+                scottsTurn = !scottsTurn;
+            }
+            else {
+                unsigned long leftThisTurn = k;
+                while (leftThisTurn > 0 && ballsLeft > 0) {
+                    rustyScore += rustyQueue.pop(otherPos);
+                    scottQueue.repair(otherPos);
+                    ballsLeft--;
+                    leftThisTurn--;
+                }
+                scottsTurn = !scottsTurn;
+            }
+        }
+        output << scottScore << " " << rustyScore << std::endl;
+    }
+
+    input.close();
+    output.close();
 
     return 0;
 }
